f2092f571d0b1b56f9ed0771149615526f3f0bd4
arhjaye <46835688+arhjaye@users.noreply.github.com>
2019-01-19 15:21:17 +0800

    Update henlohooman.py

--- henlohooman.py
+++ henlohooman.py
@@ -12,10 +12,30 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Henlo hooman! Tell me how are you feeling today! 😌")
+  bot.send_message(chat_id=update.message.chat_id, text="Henlo hooman! Tell me how are you feeling today! 😌")
+
+def isJoke(bot, update):
+  getJoke = False
+  if 'joke' in update.message.text:
+    getJoke = True
+  elif 'tell me' in update.message.text:
+    getJoke = True
+  elif 'funny' in update.message.text:
+    getJoke = True
+  elif 'sad' in update.message.text:
+    getJoke = True
+  elif 'cheer' in update.message.text:
+    getJoke = True
+  elif 'doge' in update.message.text:
+    getJoke = True
+
+  return getjoke
 
 start_handler = CommandHandler('start', start)
+joke_handler = MessageHandler(Filters.text, isJoke)
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(joke_handler)
+
 
 updater.start_polling()
