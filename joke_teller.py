c82a7cedf59631d6acbf41f8cfe70157ce6b3bb9
wailunlim <wailun@u.nus.edu>
2019-01-19 15:11:25 +0800

    add JokeTeller object

---
+++ joke_teller.py
@@ -0,0 +1,11 @@
+import random
+
+class JokeTeller():
+  def __init__(self):
+    self._jokes = [
+    'Q: What do you get when you cross a dinosaur with a pig?\n\nA: Jurrassic Pork.',
+    'Q: Why didn\'t the elephant like to play cards in the jungle?\n\nA: A: Because there were too many cheetahs.'
+    ];
+
+  def pollJokes(self):
+    return self._jokes[random.randint(0, len(self._jokes) - 1)];
