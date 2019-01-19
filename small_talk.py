452e15ef9fe23edfc9bdfedb342205092f281e5d
wailunlim <wailun@u.nus.edu>
2019-01-20 00:25:29 +0800

    enable small talk to work for different moods

--- small_talk.py
+++ small_talk.py
@@ -3,7 +3,11 @@ import random
 class SmallTalkSender():
   def __init__(self):
     with open('smallTalk/smallTalk.txt', 'r') as f:
-      self._smallTalk = f.readlines()
+      self._smallTalk = f.readlines();
 
   def pollSmallTalk(self):
     return self._smallTalk[random.randint(0, len(self._smallTalk) - 1)];
+
+  def setMood(self, moodlet):
+    with open('smallTalk/' + moodlet + '.txt', 'r') as f:
+      self._smallTalk += f.readlines();
