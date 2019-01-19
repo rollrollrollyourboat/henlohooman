47e0f0d00bcc8ec12552c0fe3888930d98e997f7
kayheen <kayheen@u.nus.edu>
2019-01-20 00:31:01 +0800

    add more images

--- image_giver.py
+++ image_giver.py
@@ -3,7 +3,7 @@ import glob
 
 class ImageGiver():
   def __init__(self):
-    self._images = glob.glob('Images/*.jpg')
+    self._images = glob.glob('Images/*.*')
 
   def pollImage(self):
     return self._images[random.randint(0, len(self._images) - 1)]
