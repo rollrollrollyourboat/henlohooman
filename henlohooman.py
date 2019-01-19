b1593460e3281e0ba0b06fbf6fc6683e697d3372
arhjaye <yow.ren.jie@gmail.com>
2019-01-19 20:45:41 +0800

    added main function to send sticker

--- henlohooman.py
+++ henlohooman.py
@@ -4,6 +4,8 @@ from telegram.ext import MessageHandler, Filters
 import joke_teller as jokes
 import joke_filter as jokeFilter
 import image_filter as images
+import sticker_filter as stickerFilter
+import send_sticker as stickers
 
 import logging
 
@@ -16,6 +18,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 joke_teller = jokes.JokeTeller();
 joke_filter = jokeFilter.JokeFilter();
+sticker_sender = stickers.StickerSender();
+sticker_filter = stickerFilter.StickerFilter();
 
 def start(bot, update):
   bot.send_message(chat_id=update.message.chat_id, text="Henlo hooman! Tell me how are you feeling today! 😌")
@@ -26,6 +30,9 @@ def imageMesg(bot, update):
   
 def getJoke(bot, update):
   bot.send_message(chat_id=update.message.chat_id, text=joke_teller.pollJoke())
+
+def sendSticker(bot, update):
+  bot.send_sticker(chat_id=update.message.chat_id, sticker=sticker_sender.pollSticker())
   
 #initialise the class
 image_filter = images.ImageFilter()
@@ -33,9 +40,11 @@ image_filter = images.ImageFilter()
 start_handler = CommandHandler('start', start)
 getJoke_handler = MessageHandler(joke_filter, getJoke)
 image_handler = MessageHandler(image_filter, imageMesg)
+sticker_handler = MessageHandler(sticker_filter, sendSticker)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(getJoke_handler)
 dispatcher.add_handler(image_handler)
+dispatcher.add_handler(sticker_handler)
 
 updater.start_polling()
