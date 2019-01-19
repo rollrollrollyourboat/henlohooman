396d7df7c6f60a31ff4ffd13f40e508c129f8d53
arhjaye <yow.ren.jie@gmail.com>
2019-01-19 20:43:51 +0800

    get sticker random sticker ID

---
+++ send_sticker.py
@@ -0,0 +1,33 @@
+import random
+
+class StickerSender():
+  def __init__(self):
+    self._stickers = [
+    'CAADBQADOgADsV0ZFMreYOeI4Ka6Ag',
+    'CAADBQADTAADsV0ZFE2qObHgdgMUAg',
+    'CAADAgADCAADNraOCMoRkz7wEy9pAg',
+    'CAADAgADCgADNraOCEl_Jsv8JOo9Ag',
+    'CAADAgADDAADNraOCEkHin2ya020Ag',
+    'CAADAgADDgADNraOCE15_R1lGtHcAg',
+    'CAADAgADEAADNraOCKnKFD435pvOAg',
+    'CAADAgADGAADNraOCFaAQzhyMG4LAg',
+    'CAADAgADFAADNraOCPLD6h0FLiZYAg',
+    'CAADAgADHAADNraOCLBipsm-lf2XAg',
+    'CAADAgADHgADNraOCE8hiCWKwKWYAg',
+    'CAADAgADIAADNraOCMcmRNa491sFAg',
+    'CAADAgADIgADNraOCDhUh5WS4N4qAg',
+    'CAADAgADJAADNraOCF04b51tkSdSAg',
+    'CAADAgADGgADNraOCNIjVxaXrZ_eAg',
+    'CAADAgADKAADNraOCCqXlVqUKd4SAg',
+    'CAADAgADPAADNraOCEH-JIBlYgrOAg',
+    'CAADAgADUQADNraOCLWtL3zC2WyxAg',
+    'CAADAgADWwADNraOCFhHT0h1zkuJAg',
+    'CAADAgADdQADNraOCC5QsW-QAAFdowI',
+    'CAADAgADgAADNraOCMyPxzxRhO2RAg',
+    'CAADBQADagEAAi1ybAF-_KGJvPaYowI',
+    'CAADBQADfgEAAi1ybAH39zBspZrTwwI',
+    'CAADBQADggEAAi1ybAFPqDtliVF_hgI'
+    ];
+
+  def pollSticker(self):
+    return self._stickers[random.randint(0, len(self._stickers) - 1)];
