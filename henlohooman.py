0f85f99ae6997184909d7e12df2ed7101e5d1707
kayheen <kayheen@u.nus.edu>
2019-01-19 20:53:15 +0800

    add functionality to send gifs

--- henlohooman.py
+++ henlohooman.py
@@ -4,6 +4,7 @@ from telegram.ext import MessageHandler, Filters
 import joke_teller as jokes
 import joke_filter as jokeFilter
 import image_filter as images
+import gif_filter as gif 
 
 import logging
 
@@ -26,16 +27,23 @@ def imageMesg(bot, update):
   
 def getJoke(bot, update):
   bot.send_message(chat_id=update.message.chat_id, text=joke_teller.pollJoke())
+
+def getGifs(bot, update):
+  link = "https://media.tenor.com/images/6f61d7cc7cecb9c2046f4baf0e71d006/tenor.gif"
+  bot.send_animation(chat_id=update.message.chat_id, animation=link)
   
 #initialise the class
 image_filter = images.ImageFilter()
+gif_filter = gif.GIFFilter()
 
 start_handler = CommandHandler('start', start)
 getJoke_handler = MessageHandler(joke_filter, getJoke)
 image_handler = MessageHandler(image_filter, imageMesg)
+gif_handler = MessageHandler(gif_filter, getGifs)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(getJoke_handler)
 dispatcher.add_handler(image_handler)
+dispatcher.add_handler(gif_handler)
 
 updater.start_polling()
