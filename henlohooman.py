a78d7e59751cd746e2a94cec8815abe9371628bc
wailunlim <wailun@u.nus.edu>
2019-01-19 15:46:49 +0800

    update main to contain joketeller object

--- henlohooman.py
+++ henlohooman.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+import joke_teller as jokes
 
 import logging
 
@@ -9,10 +10,12 @@ updater = Updater(token='780805165:AAEdreGeX_G1ECiU6dWo2cF9UHrk545agAw')
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+        level=logging.INFO)
+
+joke_teller = jokes.JokeTeller();
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Henlo hooman! Tell me how are you feeling today! 😌")
+  bot.send_message(chat_id=update.message.chat_id, text="Henlo hooman! Tell me how are you feeling today! 😌")
 
 start_handler = CommandHandler('start', start)
 
